6511f48f696b1441751859ff165a9b2f9f97e98d
Thiha Soe Htet <thihasoehtet.isn@gmail.com>
2014-07-09 00:02:27 +0800

    Add Proj

---
+++ StartStopRestart/StartStopRestart/Controller.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartStopRestart
+{
+    public class Controller
+    {
+        private int _count = 0;
+        private Task _job;
+        private bool _isStopped = false;
+
+        public void Start()
+        {
+            _count++;
+            _isStopped = false;
+            _job = DoJob(_count);
+        }
+
+        private async Task DoJob(int count)
+        {
+            while (!_isStopped)
+            {
+                Console.WriteLine("Doing Job {0}...", count);
+                await Task.Delay(TimeSpan.FromSeconds(0.5));
+            }
+        }
+
+        public void Stop()
+        {
+            _isStopped = true;
+            _job.Wait();
+            Console.WriteLine("Job {0} stopped.", _count);            
+        }
+
+        public void Restart()
+        {
+            this.Stop();
+            this.Start();
+        }
+    }
+}
---
+++ StartStopRestart/StartStopRestart/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartStopRestart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Controller c = new Controller();
+            c.Start();
+            Console.ReadKey();
+            c.Restart();
+            Console.ReadKey();
+        }
+    }
+}
